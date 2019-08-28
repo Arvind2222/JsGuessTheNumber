9030ef44944dd1ab823ccd3b54416977fdf82129
ysumit99 <ysumit99@gmail.com>
2019-08-28 19:40:12 +0530

    Layout for Levels, Guess Result added!

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,20 @@
-
+    
+    /**
+     * This method alerts/logs the guessed number.
+     */
     let popUp = () => {
        
         let guessedValue = document.getElementById("inputBox").value;
 
         console.log("Guessed Number => " + guessedValue);
-        alert("guessed Number => " + guessedValue);
+        // alert("guessed Number => " + guessedValue);
+
+        $('#guessResult').html(`You guessed ${guessedValue}`);
     }
 
+    /**
+     * click eventListener for the submit button.
+     */
     document.querySelector("#submit").addEventListener("click", popUp);
 
 
