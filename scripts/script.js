b2dc286735b7d0c55776b56bd5e37e34ede4d1d0
ysumit99 <ysumit99@gmail.com>
2019-08-29 22:36:58 +0530

    dynamically changing emotion based on guess!

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,80 @@
+    /* Global Variable  */
+    let randNumber = 0;
+
     
     /**
-     * This method alerts/logs the guessed number.
+     * This method checks if guess is correct or incorrect
+     * 
      */
-    let popUp = () => {
+    let checkGuess = () => {
        
         let guessedValue = document.getElementById("inputBox").value;
 
-        console.log("Guessed Number => " + guessedValue);
-        // alert("guessed Number => " + guessedValue);
+        
+        console.log("Guessed Value = " + guessedValue + " | Random Number = " + randNumber);
 
-        $('#guessResult').html(`You guessed ${guessedValue}`);
+        /* Unhide ResultArea div */
+        $('#resultArea').css({"display":"block"});
+
+        if(guessedValue != randNumber)
+            {
+                
+
+                $('#guessResultImage').attr("src", "images/sad.svg");  /* sad emotion as guessed incorrectly */
+
+                $('#guessVerdict').html('<span> You guessed a little higher/lower</span>'); /* display verdict */
+
+                $('#guessVerdict').css({"color":"red"});
+
+                $('#nextLevelButton').css({"display":"none"});
+            }
+        else
+            {   
+                
+                $('#guessResultImage').attr("src", "images/pumpkin.svg"); /* happy emotion as guessed correctly! */
+
+                $('#guessVerdict').html('<span>Congrats! You made it ! Try next Level </span>')/* display vardict */
+
+                $('#guessVerdict').css({"color":"green"});
+
+
+                $('#nextLevelButton').toggle();
+            }
+
+        return guessedValue;
     }
 
+
+     /**
+      * Random Number  Generator between 1 and 100
+      * 
+      */
+     let randomNumber = () => {
+        let rand = Math.floor((Math.random()*100));
+
+        return rand;
+    }
+
+
+   
     /**
      * click eventListener for the submit button.
+     * 
      */
-    document.querySelector("#submit").addEventListener("click", popUp);
+    let guessvalue = document.querySelector("#submit").addEventListener("click", checkGuess);
+    
+    
 
 
+    /**
+     * Create Random Number only after document is loaded completely
+     * 
+     */
+    $(document).ready(function(){
 
+        randNumber = randomNumber(); 
+
+        console.log("generated Random Number on document.ready => " + randNumber);
+     
+    });
+   
