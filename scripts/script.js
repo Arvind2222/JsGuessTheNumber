baaddcce07088a1ed2986da437bd9460a4a705a3
Sumit Yadav <ysumit99@gmail.com>
2019-09-03 14:14:15 +0530

    Update script.js
    Turns out Math.random() never returns '1'.  Reference MDN web docs (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)

--- scripts/script.js
+++ scripts/script.js
@@ -125,7 +125,7 @@
      */
 
     function getRandomInt(min = 1, max = 100) {
-        return Math.floor(Math.random() * (max - min + 1));
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
     let randomNumber = () => {
